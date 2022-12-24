05635330341a3aa6087394cde480af9bc2695f11
ThinkiumChain <Thinkglobal888>
2022-12-25 01:47:29 +0800

    sub component

---
+++ src/components/HiVue.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div id="HiVue">
+    <h1 class="styh1">Hi Vue！！！,{{name}} loves {{age}}</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "HiVue",
+  // props:['name','hoppy']
+  props: {
+    name:String,
+    age:[Boolean,Number],
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
